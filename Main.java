7864f10f751e7a53b1750f868a4d9758d1b42773
weioon <weioon0509@gmail.com>
2024-12-03 14:33:09 +0800

    update

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main{
-    public static void Main(String [] args){
+    public static void main(String [] args){
         String name = "Tok";
         System.out.println(name);
     }
---
+++ SwitchCase.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class SwitchCase {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int dayNum;
+        System.out.print("Please enter the day name");
+        dayNum = scan.nextInt();
+        switch(dayNum){
+            case 1:
+            System.out.println("The number that you enter is Monday");
+            break;
+            case 2:
+            System.out.println("The number that you enter is Tuesday");
+            break;
+            case 3:
+            System.out.println("The number that you enter is Wednesday");
+            break;
+            case 4:
+            System.out.println("The number that you enter is Thursday");
+            break;
+            case 5:
+            System.out.println("The number that you enter is Friday");
+            break;
+            case 6:
+            System.out.println("The number that you enter is Saturday");
+            break;
+            case 7:
+            System.out.println("The number that you enter is Sunday");
+            break;
+            default:
+            System.out.println("The number that you enter is invalid day. Please try again!");
+        }
+    }
+    
+}
